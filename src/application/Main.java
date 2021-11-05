eba1bd34014d9c1b614c0b004f28c6bf8a8c80e2
Roberto Mora <robertoluismoraromero>
2021-11-05 10:14:25 +0100

    cambio

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,7 +2,11 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Persona;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -12,10 +16,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("Agenda de Contactos");
+			Scene scene = new Scene(root,655,494.0);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			Persona Paco = new Persona();
+			Paco.setNombre("Paco");
+			Paco.setApellido("Gómez");
+			Paco.setCalle("Avenida de España");
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
